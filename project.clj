6773f41b6b75ba320561029a79d4e9a180d2ba52
Ning Sun <sunng@about.me>
2017-12-20 00:59:35 +0800

    (chore) update dependencies
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://github.com/sunng87/slacker-htrace"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[slacker "0.15.0"]
+  :dependencies [[slacker "0.16.0"]
                  [org.apache.htrace/htrace-core4 "4.2.0-incubating"]]
   :profiles {:example {:source-paths ["examples"]}
              :zipkin-example {:dependencies [[org.apache.htrace/htrace-zipkin "4.2.0-incubating"]]}
-             :dev {:dependencies [[org.clojure/clojure "1.8.0"]]}}
+             :dev {:dependencies [[org.clojure/clojure "1.9.0"]]}}
   :deploy-repositories {"releases" :clojars}
   :aliases {"run-example" ["trampoline" "with-profile" "default,example" "run" "-m" "slacker-htrace.main"]
             "run-zipkin-example" ["trampoline" "with-profile" "default,example,zipkin-example" "run" "-m" "slacker-htrace.main"]})
